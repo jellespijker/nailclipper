4df52375a03dcc0eb831c0535ccf458001daeae7
Jelle Spijker <spijker.jelle@gmail.com>
2022-07-03 17:33:38 +0200

    Add container set Number to default to double

--- include/NailClipper/Types.h
+++ include/NailClipper/Types.h
@@ -4,30 +4,42 @@
 #ifndef NAILCLIPPER_TYPES_H
 #define NAILCLIPPER_TYPES_H
 
-#include <vector>
 #include <array>
+#include <vector>
 
 #include "NailClipper/Concepts.h"
 
 namespace nail
 {
-template<Number T>
+template<Number T = double>
 using point2d_t = std::array<T, 2>;
 
-template<Number T>
+template<Number T = double>
 using point3d_t = std::array<T, 3>;
 
-template<Number T>
+template<Number T = double>
 using polyline2d_t = std::vector<point2d_t<T>>;
 
-template<Number T>
+template<Number T = double>
+using polygon2d_t = std::vector<point2d_t<T>>;
+
+template<Number T = double>
 using polyline3d_t = std::vector<point3d_t<T>>;
 
+template<Number T = double>
+using polygon3d_t = std::vector<point3d_t<T>>;
+
+template<Number T = double>
+using container_2d_t = std::vector<polygon2d_t<T>>;
+
+template<Number T = double>
+using container_3d_t = std::vector<polygon3d_t<T>>;
+
 #ifdef WITH_MESH
-template<Number T>
+template<Number T = double>
 using face_t = std::vector<point3d_t<T>>;
 
-template<Number T>
+template<Number T = double>
 using mesh_t = std::vector<face_t<T>>;
 #endif
 } // namespace nail
