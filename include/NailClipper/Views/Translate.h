57a1dda49de9c9fb5f90f10c4f6f91347f32a844
Jelle Spijker <spijker.jelle@gmail.com>
2022-07-16 14:08:24 +0200

    Added Scale, Translate and general transform views
    Usage like:
    
    ```c++
    std::array<double, 2> vec{ 200.0, 10.0 };
    std::array<double, 2> scale_vec{ 2.0, 2.0 };
    std::vector<std::array<double, 2>> poly{ { 3.0, 4.0 }, { 6.0, 9.0 }, { 5.5, 1.25 } };
    
    auto translated = poly | views::translate(vec) | views::scale(scale_vec);
    
    for (const auto& p : translated)
    {
    ...
    }
    ```

---
+++ include/NailClipper/Views/Translate.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 Jelle Spijker
+// NailClipper is released under the terms of the AGPLv3 or higher
+
+#ifndef NAILCLIPPER_SCALE_H
+#define NAILCLIPPER_SCALE_H
+
+#include "NailClipper/Concepts.h"
+#include "NailClipper/Views/Transform.h"
+#include <range/v3/functional/arithmetic.hpp>
+
+namespace nail::views
+{
+inline constexpr auto translate(Point auto& vec)
+{
+    return ranges::make_view_closure(
+      [&vec](auto&& rng)
+      { return detail::transform_fn(std::forward<decltype(rng)>(rng), vec, std::forward<ranges::plus>(ranges::plus{})); });
+}
+} // namespace nail::views
+
+#endif // NAILCLIPPER_SCALE_H
